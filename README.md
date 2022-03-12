a8cf77bc3bf18b7be6267a55c94c38ceadcb139c
yeswici <gye@yeswici.com>
2022-03-12 10:46:16 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-f1rpv1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-f1rpv1)
---
+++ src/app/cdk-drag-drop-disabled-sorting-example.ts
@@ -0,0 +1,34 @@
+import {Component} from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+
+/**
+ * @title Drag&Drop disabled sorting
+ */
+@Component({
+  selector: 'cdk-drag-drop-disabled-sorting-example',
+  templateUrl: 'cdk-drag-drop-disabled-sorting-example.html',
+  styleUrls: ['cdk-drag-drop-disabled-sorting-example.css'],
+})
+export class CdkDragDropDisabledSortingExample {
+  items = ['Carrots', 'Tomatoes', 'Onions', 'Apples', 'Avocados'];
+
+  basket = ['Oranges', 'Bananas', 'Cucumbers'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+}
+
+
+/**  Copyright 2022 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
